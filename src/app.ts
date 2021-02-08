1c8960f2d2e227fa64c0b3da170a025905db8fee
Chamith95 <chamithjay@gmail.com>
2021-02-09 01:11:56 +0530

    webpack added

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
 // Drag and Drop Intefaces
 
-import { ProjectInput } from "./components/project-input.js";
-import { ProjectList } from "./components/project-list.js";
+import { ProjectInput } from "./components/project-input";
+import { ProjectList } from "./components/project-list";
 
 
 new ProjectInput();
